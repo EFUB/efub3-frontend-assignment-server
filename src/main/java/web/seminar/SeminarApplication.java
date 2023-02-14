8a775a286d3e54660f571af3deb4457d1b00ad3d
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-02-14 15:54:20 +0900

    💎 FIRST COMMIT

---
+++ src/main/java/web/seminar/SeminarApplication.java
@@ -0,0 +1,15 @@
+package web.seminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarApplication.class, args);
+	}
+
+}
